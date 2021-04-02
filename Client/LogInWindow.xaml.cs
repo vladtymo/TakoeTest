c301adc3e344f2b29faeafdad2dc06214fd0fc74
Roman131192 <romanmartyniuk92@gmail.com>
2021-04-02 20:12:02 +0300

    update login windows and create login view model

---
+++ Client/LogInWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for LogInWindow.xaml
+    /// </summary>
+    public partial class LogInWindow : Window
+    {
+        private LogInViewModel viewModel = new LogInViewModel();
+        public LogInWindow()
+        {
+            InitializeComponent();
+            this.DataContext = viewModel;
+        }
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.DataContext != null)
+            {
+                ((dynamic)this.DataContext).Password = ((PasswordBox)sender).Password;
+            }
+        }
+
+        private void Label_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
