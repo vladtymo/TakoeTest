e24711f30b3cfeeab088f3a695712cf93956eeb0
K0L9 <kovalkola2@gmai.com>
2021-04-04 13:20:17 +0300

    Create services for creating test entities (category, test, question(empty), answer(empty)).

---
+++ WcfService/Interfaces/ITestService.cs
@@ -0,0 +1,25 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ITestService" in both code and config file together.
+	[ServiceContract]
+	public interface ITestService
+	{
+		[OperationContract]
+		void AddTest(Test test);
+		[OperationContract]
+		bool IsTextNameExist(string name);
+
+		[OperationContract]
+		Test GetTestById(int id);
+		[OperationContract]
+		Test GetTestByName(string name);
+	}
+}
---
+++ WcfService/Services/CategoryService.svc.cs
@@ -0,0 +1,40 @@
+﻿using DAL;
+using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CategoryService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CategoryService.svc or CategoryService.svc.cs at the Solution Explorer and start debugging.
+    public class CategoryService : ICategoryService
+    {
+        UnitOfWork unit;
+
+        public CategoryService()
+        {
+            unit = new UnitOfWork();
+        }
+
+        public void AddCategory(Category category)
+        {
+            unit.CategoryRepos.Insert(category);
+            unit.Save();
+        }
+        public bool IsCategoryNameExist(string name) => unit.CategoryRepos.Get(c => c.Name == name).Count() != 0;
+
+        public Category GetCategoryById(int id) => unit.CategoryRepos.GetById(id);
+        public Category GetCategoryByName(string name) => unit.CategoryRepos.Get(c => c.Name == name).SingleOrDefault();
+
+        public void AddTestToCategory(Category category, Test testToAdd)
+        {
+            category.Tests.Append(testToAdd);
+            unit.CategoryRepos.Update(category);
+            unit.Save();
+        }
+    }
+}
