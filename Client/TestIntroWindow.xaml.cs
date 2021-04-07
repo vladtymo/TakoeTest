efa547d80139afc64dc8d46a78b93fac5603e316
Саня Жигула <zhyhy123@gmail.com>
2021-04-07 20:06:53 +0300

    Window Created

---
+++ Client/TestIntroWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for TestIntroWindow.xaml
+    /// </summary>
+    public partial class TestIntroWindow : Window
+    {
+        public TestIntroWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            listTopPeopleTest.Items.Add(new TopPeopleTestViewModel() { FullName = "Good Man", Time = "12:15", CorrectAnswer = "10/10", Mark = "10" });
+        }
+    }
+}
---
+++ Client/TopPeopleTestViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class TopPeopleTestViewModel : INotifyPropertyChanged
+    {
+        private string fullName;
+        public string FullName
+        {
+            get { return fullName; }
+            set
+            {
+                if (fullName != value)
+                {
+                    fullName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string time;
+        public string Time
+        {
+            get { return time; }
+            set
+            {
+                if (time != value)
+                {
+                    time = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string correctAnswer;
+        public string CorrectAnswer
+        {
+            get { return correctAnswer; }
+            set
+            {
+                if (correctAnswer != value)
+                {
+                    correctAnswer = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string mark;
+        public string Mark
+        {
+            get { return mark; }
+            set
+            {
+                if (mark != value)
+                {
+                    mark = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
