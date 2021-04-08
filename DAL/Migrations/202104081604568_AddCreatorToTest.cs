42bb3b0ef2cf8daa5ef757a6cf1ffe8ae5b8d7f1
Kolya Koval <kovalkola2@gmail.com>
2021-04-08 20:51:58 +0300

    BIG bug FIX

---
+++ DAL/Migrations/202104081604568_AddCreatorToTest.cs
@@ -0,0 +1,20 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCreatorToTest : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Users", "Gender", c => c.Int(nullable: false));
+            DropColumn("dbo.Users", "IsMale");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Users", "IsMale", c => c.Boolean());
+            DropColumn("dbo.Users", "Gender");
+        }
+    }
+}
